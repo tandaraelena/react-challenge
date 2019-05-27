a630d5b6585d00791bc27d9437e9484161dc3482
Tandara Elena <tandaraelena@yahoo.com>
2019-05-27 14:57:17 +0100

    added avatar images style them and used array list to loop thru

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+const App = () => <PersonList />;
+const PersonList = () => {
+  const people = [
+    {
+      img: 1,
+      name: "john",
+      job: "developer"
+    },
+    {
+      img: 2,
+      name: "bob",
+      job: "designer"
+    },
+    {
+      img: 3,
+      name: "peter",
+      job: "musician"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]} />
+      <Person person={people[2]} />
+    </section>
+  );
+};
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+
+  const url = require(`./images/avatar${img}.png`);
+  return (
+    <div className="person">
+      <img src={url} alt="avatar" />
+      <h3>{name}</h3>
+      <h4>{job}</h4>
     </div>
   );
-}
+};
 
 export default App;
