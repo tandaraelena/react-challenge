56f99c2cc77c368edfde2b5df96c9d2fbcaf7c3a
Tandara Elena <tandaraelena@yahoo.com>
2019-05-27 17:14:22 +0100

    add more content and apply different style

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ const PersonList = () => {
     }
   ];
   return (
-    <section>
-      <Person person={people[0]} />
-      <Person person={people[1]} />
-      <Person person={people[2]} />
+    <section className="personList">
+      <Person person={people[0]}>
+        Lorem ipsum dolor sit amet, duo unum ludus cu, pri ne minim virtute
+        mediocritatem. Id vis magna lorem impedit, an volumus eligendi appareat
+        pri, vim aperiam perpetua no.
+      </Person>
+      <Person person={people[1]}>
+        Vocibus torquatos sadipscing an est. Mutat option periculis pro eu, an
+        tota integre impedit sed, ne per graeco argumentum persequeris. Integre
+        salutatus evertitur ad eos. Sed nullam corrumpit ad, vidisse molestiae
+        per id, ex elit mutat nonumes mei.
+      </Person>
+      <Person person={people[2]}>
+        Meis harum alterum sea et. Molestiae concludaturque mei te, cu eam mazim
+        electram. Aliquam euripidis has an, nec no tempor recteque gubergren, eu
+        vim platonem principes. Eam eu nulla omittantur, vix at tantas facilisis
+        aliquando.
+      </Person>
     </section>
   );
 };
@@ -36,8 +50,9 @@ const Person = props => {
   return (
     <div className="person">
       <img src={url} alt="avatar" />
-      <h3>{name}</h3>
+      <h4>{name}</h4>
       <h4>{job}</h4>
+      {children}
     </div>
   );
 };
